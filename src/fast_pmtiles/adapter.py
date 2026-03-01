7cad764560d0e637885445ab18bfb68b6fc08e54
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2026-02-28 20:29:56 -0500

    add comment with source

--- src/fast_pmtiles/adapter.py
+++ src/fast_pmtiles/adapter.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 from aiohttp import ClientSession
 from async_pmtiles import Store
 
+# source:
+# https://github.com/developmentseed/async-pmtiles/blob/829fbe3c2b09c931afb77462133e70ecc68a3ba9/README.md
+
 
 @dataclass
 class AiohttpAdapter(Store):
@@ -16,6 +19,6 @@ class AiohttpAdapter(Store):
         length: int,
     ) -> bytes:
         inclusive_end = start + length - 1
-        headers = {"Range": f"bytes={start}-{inclusive_end}"}
+        headers: dict[str, str] = {"Range": f"bytes={start}-{inclusive_end}"}
         async with self.session.get(path, headers=headers) as response:
             return await response.read()
